d21cf8f2e69bb7b606c7e504fbb216256acf09ce
harryish <36301571+harryish@users.noreply.github.com>
2018-03-19 14:45:36 +0530

    Update svm_author_id.py

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -26,5 +26,8 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 
 #########################################################
-
+from sklearn import svm
+clf=svm.SVC()
+clf.fit(features_train,labels_train)
+perd=clf.predict()
 
